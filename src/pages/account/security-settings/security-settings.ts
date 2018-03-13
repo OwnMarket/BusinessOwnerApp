b32c219e79aae5faebfdebe4ff2226c92ad1d757
Yablan <yablan041@outlook.com>
2018-03-14 00:45:22 +0100

    security settings save

--- src/pages/account/security-settings/security-settings.ts
+++ src/pages/account/security-settings/security-settings.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UserProvider } from '../../../providers/user.provider';
+import { ToastMessageProvider } from '../../../providers/toast-message.provider';
+import { ChangePasswordModel } from '../../../models/change-password.model';
 
 /**
  * Generated class for the SecuritySettingsPage page.
@@ -13,12 +16,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'security-settings.html',
 })
 export class SecuritySettingsPage {
+  model: ChangePasswordModel = new ChangePasswordModel();
+  confirmPassword: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private _userProvider: UserProvider,
+    private _toastr: ToastMessageProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SecuritySettingsPage');
+  changePassword() {
+    if (this.confirmPassword == this.model.newPassword) {
+      this._userProvider.changePassword(this.model)
+        .subscribe((data) => {
+          this._toastr.success('Security settings updated!');
+        }, (err) => {
+          this._toastr.errorAlerts(err);
+        }, () => {
+          this.model.currentPassword = '';
+          this.model.newPassword = '';
+          this.confirmPassword = '';
+        });
+    }else{
+      this._toastr.error('"New password" and "Confirm password" must match!');
+    }
   }
 
 }
